aecf9b06dfa1327cc351cd493d3163ef4bf6fc93
Merlin Westphal <email@merlinw.de>
2019-03-03 15:55:55 +0100

    + Moved constructor logic into enable-eventhandler

--- src/main/java/net/twasiplugin/timedmessages/TwasiUserPlugin.java
+++ src/main/java/net/twasiplugin/timedmessages/TwasiUserPlugin.java
@@ -3,6 +3,7 @@ package net.twasiplugin.timedmessages;
 import net.twasi.core.events.TwasiEventHandler;
 import net.twasi.core.plugin.api.TwasiCustomCommand;
 import net.twasi.core.plugin.api.events.TwasiDisableEvent;
+import net.twasi.core.plugin.api.events.TwasiEnableEvent;
 import net.twasi.core.plugin.api.events.TwasiInstallEvent;
 import net.twasi.core.services.ServiceRegistry;
 import net.twasiplugin.dependency.streamtracker.StreamTrackerService;
@@ -14,7 +15,6 @@ import net.twasiplugin.timedmessages.commands.TimerCommand;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 public class TwasiUserPlugin extends net.twasi.core.plugin.api.TwasiUserPlugin {
 
@@ -34,34 +34,28 @@ public class TwasiUserPlugin extends net.twasi.core.plugin.api.TwasiUserPlugin {
         e.getModeratorsGroup().removeKey("twasi.timer.list");
         e.getModeratorsGroup().removeKey("twasi.timer.enable");
         e.getModeratorsGroup().removeKey("twasi.timer.disable");
+        Plugin.service.stopTimers(TwasiUserPlugin.this);
     }
 
-    public TwasiUserPlugin() {
-        Thread t1 = new Thread(() -> {
-            try {
-                TimeUnit.SECONDS.sleep(5);
-            } catch (InterruptedException ignored) {
+    @Override
+    public void onEnable(TwasiEnableEvent e) {
+        commands.add(new TimerCommand(this));
+        commands.add(new AddTimerCommand(this));
+        commands.add(new DelTimerCommand(this));
+        StreamTrackerService sts = ServiceRegistry.get(StreamTrackerService.class);
+        sts.registerStreamTrackEvent(this.getTwasiInterface().getStreamer().getUser(), new StreamTrackerService.TwasiStreamTrackEventHandler() {
+            @Override
+            public void on(StreamTrackEvent streamTrackEvent) {
+                if (!Plugin.service.hasTimersEnabled(getTwasiInterface().getStreamer().getUser()))
+                    Plugin.service.startTimers(TwasiUserPlugin.this);
             }
-            commands.add(new TimerCommand(this));
-            commands.add(new AddTimerCommand(this));
-            commands.add(new DelTimerCommand(this));
-            StreamTrackerService sts = ServiceRegistry.get(StreamTrackerService.class);
-            sts.registerStreamTrackEvent(this.getTwasiInterface().getStreamer().getUser(), new StreamTrackerService.TwasiStreamTrackEventHandler() {
-                @Override
-                public void on(StreamTrackEvent streamTrackEvent) {
-                    if (!Plugin.service.hasTimersEnabled(getTwasiInterface().getStreamer().getUser()))
-                        Plugin.service.startTimers(TwasiUserPlugin.this);
-                }
-            });
-            sts.registerStreamStopEvent(this.getTwasiInterface().getStreamer().getUser(), new TwasiEventHandler<StreamStopEvent>() {
-                @Override
-                public void on(StreamStopEvent streamStopEvent) {
-                    Plugin.service.stopTimers(TwasiUserPlugin.this);
-                }
-            });
         });
-        t1.setDaemon(true);
-        t1.start();
+        sts.registerStreamStopEvent(this.getTwasiInterface().getStreamer().getUser(), new TwasiEventHandler<StreamStopEvent>() {
+            @Override
+            public void on(StreamStopEvent streamStopEvent) {
+                Plugin.service.stopTimers(TwasiUserPlugin.this);
+            }
+        });
     }
 
     @Override
